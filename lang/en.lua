09774205f76fe078dfdd49426273f490eed2eb07
Void <66706132+Near717@users.noreply.github.com>
2023-11-04 04:55:57 -0300

    Update en.lua
    revised strings

--- lang/en.lua
+++ lang/en.lua
@@ -1,11 +1,11 @@
 local strings = {
-    SI_BINDING_NAME_NSR_toggle        = 'Toggle window',
-    SI_BINDING_NAME_NSR_toggle2       = 'Toggle quick window',
+    SI_BINDING_NAME_NSR_toggle        = 'Toggle Main window',
+    SI_BINDING_NAME_NSR_toggle2       = 'Toggle Quick view window',
 
     NEARSR_nodata			= 'No data is available for the selected character.\nPlease log in with that character first.',
     NEARSR_notdiscovered	= 'Skill line not discovered',
 
-    NEARSR_quick            = 'Show skill line ranks quick list',
+    NEARSR_quick            = 'Show "Skill Line" ranks in Quick view',
 }
 
 for stringId, stringValue in pairs(strings) do
